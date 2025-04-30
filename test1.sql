d99b7fc9bc4ecad71591376132151b985a98f9e3
gunstp <Peter.gunston@nhs.net>
2025-04-30 15:08:54 +0000

    End of course

--- test1.sql
+++ test1.sql
@@ -1,4 +1,7 @@
 --select * from airports
+--Select * FROM PatientStay
+
+
 
 select 
     ps.PatientId
@@ -14,5 +17,40 @@ WHERE ps.Hospital IN ('pruh', 'oxleas')
 And ps.Ward Like '%Surgery'
 --And ps.AdmittedDate >= '2024-02-27'
 --and ps.AdmittedDate <= '2024-03-01'
-And ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+--And ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+ORDER BY ps.AdmittedDate DESC, ps.PatientId DESC
 
+SELECT 
+ps.Hospital
+, ps.Ward
+, COUNT(*) AS Numberofpatients 
+,sum(ps.Tariff) AS Tarriff
+,AVG(ps.Tariff) AS Avgtariff
+,max(ps.Tariff) AS MAXtariff 
+,Min(ps.Tariff) AS MAXtariff
+FROM PatientStay PS
+GROUP BY ps.Hospital, ps.Ward
+--Order by ps.Hospital, ps.Ward
+order by numberofpatients DESC
+ 
+
+--SELECT 
+    --ps.Ward,
+    --AVG(ps.Tariff) AS AverageTariff
+--FROM PatientStay ps
+--GROUP BY ps.Ward
+--WHERE ps.Hospital IN ('pruh', 'oxleas')
+  --AND ps.Ward LIKE '%Surgery';
+ 
+  SELECT * FROM PatientStay ps JOIN DimHospital DH on ps.Hospital = DH.Hospital
+
+SELECT
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,ps.Hospital
+    ,dh.HospitalType
+    ,dh.HospitalSize
+FROM PatientStay ps left JOIN DimHospitalBad dh ON ps.hospital = dh.Hospital
+where DH.Hospital IS NULL
+
+Select * from DimHospitalBad
